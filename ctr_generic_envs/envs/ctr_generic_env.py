984f46441ed59ef1ab731083a14dee073f348606
Keshav Iyengar <keshav.iyengar94@gmail.com>
2021-09-07 15:08:25 +0100

    readded evaluation infos.

--- ctr_generic_envs/envs/ctr_generic_env.py
+++ ctr_generic_envs/envs/ctr_generic_env.py
@@ -162,16 +162,17 @@ class CtrGenericEnv(gym.GoalEnv):
         obs = self.rep_obj.get_obs(desired_goal, achieved_goal, self.goal_tol_obj.get_tol(), self.system_idx)
         if self.evaluation:
             # Evaluation infos
-            info = {'is_success': (np.linalg.norm(desired_goal - achieved_goal) < self.goal_tol_obj.get_tol()),
-                    'error': np.linalg.norm(desired_goal - achieved_goal)}
             #info = {'is_success': (np.linalg.norm(desired_goal - achieved_goal) < self.goal_tol_obj.get_tol()),
-            #        'errors_pos': np.linalg.norm(desired_goal - achieved_goal),
-            #        'errors_orient': 0,
-            #        'position_tolerance': self.goal_tol_obj.get_tol(),
-            #        'orientation_tolerance': 0,
-            #        'achieved_goal': achieved_goal,
-            #        'desired_goal': desired_goal, 'starting_position': self.starting_position,
-            #        'q_desired': self.desired_q, 'q_achieved': self.rep_obj.get_q(), 'q_starting': self.starting_joints}
+            #        'error': np.linalg.norm(desired_goal - achieved_goal)}
+            info = {'is_success': (np.linalg.norm(desired_goal - achieved_goal) < self.goal_tol_obj.get_tol()),
+                    'errors_pos': np.linalg.norm(desired_goal - achieved_goal),
+                    'errors_orient': 0,
+                    'system_idx': self.system_idx,
+                    'position_tolerance': self.goal_tol_obj.get_tol(),
+                    'orientation_tolerance': 0,
+                    'achieved_goal': achieved_goal,
+                    'desired_goal': desired_goal, 'starting_position': self.starting_position,
+                    'q_desired': self.desired_q, 'q_achieved': self.rep_obj.get_q(), 'q_starting': self.starting_joints}
         else:
             info = {'is_success': (np.linalg.norm(desired_goal - achieved_goal) < self.goal_tol_obj.get_tol()),
                     'error': np.linalg.norm(desired_goal - achieved_goal)}
