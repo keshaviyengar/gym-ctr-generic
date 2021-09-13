7ce135e2b64f8fe3f307710230353f1db4b55608
Keshav Iyengar <keshav.iyengar94@gmail.com>
2021-09-13 14:04:53 +0100

    added systems from literature.

--- ctr_generic_envs/__init__.py
+++ ctr_generic_envs/__init__.py
@@ -5,38 +5,72 @@ register(
     id='CTR-Generic-Reach-v0', entry_point='ctr_generic_envs.envs:CtrGenericEnv',
     kwargs={
         'ctr_systems': {
+            # Autonomous steering by Mohsen Khadem
             'ctr_0': {
                 'tube_0':
-                    {'length': 215e-3, 'length_curved': 14.9e-3, 'diameter_inner': 1.0e-3, 'diameter_outer': 2.4e-3,
-                     'stiffness': 5e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 15.82, 'y_curvature': 0
+                    {'length': 431e-3, 'length_curved': 103e-3, 'diameter_inner': 0.7e-3, 'diameter_outer': 1.10e-3,
+                     'stiffness': 10.25e+10, 'torsional_stiffness': 18.79e+10, 'x_curvature': 21.3, 'y_curvature': 0
                      },
 
                 'tube_1':
-                    {'length': 120.2e-3, 'length_curved': 21.6e-3, 'diameter_inner': 3.0e-3, 'diameter_outer': 3.8e-3,
-                     'stiffness': 5e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 11.8, 'y_curvature': 0
+                    {'length': 332e-3, 'length_curved': 113e-3, 'diameter_inner': 1.4e-3, 'diameter_outer': 1.8e-3,
+                     'stiffness': 68.6e+10, 'torsional_stiffness': 11.53e+10, 'x_curvature': 13.1, 'y_curvature': 0
                      },
 
                 'tube_2':
-                    {'length': 48.5e-3, 'length_curved': 8.8e-3, 'diameter_inner': 4.4e-3, 'diameter_outer': 5.4e-3,
-                     'stiffness': 5.0e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 20.04, 'y_curvature': 0
+                    {'length': 174e-3, 'length_curved': 134e-3, 'diameter_inner': 2e-3, 'diameter_outer': 2.4e-3,
+                     'stiffness': 16.96e+10, 'torsional_stiffness': 14.25e+10, 'x_curvature': 3.5, 'y_curvature': 0
                      }
             },
+            # Learning the FK and IK of a 6-DOF CTR by Grassmann
             'ctr_1': {
                 'tube_0':
-                    {'length': 150e-3, 'length_curved': 100e-3, 'diameter_inner': 1.0e-3, 'diameter_outer': 2.4e-3,
-                     'stiffness': 5e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 15.82, 'y_curvature': 0,
+                    {'length': 370e-3, 'length_curved': 45e-3, 'diameter_inner': 0.3e-3, 'diameter_outer': 0.4e-3,
+                     'stiffness': 50e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 15.8, 'y_curvature': 0,
                      },
 
                 'tube_1':
-                    {'length': 100e-3, 'length_curved': 21.6e-3, 'diameter_inner': 3.0e-3, 'diameter_outer': 3.8e-3,
-                     'stiffness': 5e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 11.8, 'y_curvature': 0,
+                    {'length': 305e-3, 'length_curved':100e-3, 'diameter_inner': 0.7e-3, 'diameter_outer': 0.9e-3,
+                     'stiffness': 50e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 9.27, 'y_curvature': 0,
                      },
 
                 'tube_2':
-                    {'length': 70e-3, 'length_curved': 8.8e-3, 'diameter_inner': 4.4e-3, 'diameter_outer': 5.4e-3,
-                     'stiffness': 5.0e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 20.04, 'y_curvature': 0,
+                    {'length': 170e-3, 'length_curved': 100e-3, 'diameter_inner': 1.2e-3, 'diameter_outer': 1.5e-3,
+                     'stiffness': 50e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 4.37, 'y_curvature': 0,
                      }
+            },
+            # RViM lab tube parameters
+            'ctr_2': {
+                'tube_0':
+                    {'length': 309e-3, 'length_curved': 145e-3, 'inner_diameter': 0.7e-3, 'outer_diameter': 1.1e-3,
+                     'stiffness': 75e+9, 'torsional_stiffness': 25e+9, 'x_curvature': 13.52, 'y_curvature': 0
+                     },
+                'tube_1':
+                    {'length': 275e-3, 'length_curved': 114e-3, 'inner_diameter': 1.4e-3, 'outer_diameter': 1.8 - 3,
+                    'stiffness': 75e+9, 'torsional_stiffness': 25e+9, 'x_curvature': 11.68, 'y_curvature': 0
+                     },
+               'tube_2':
+                   {'length': 173e-3, 'length_curved': 173e-3, 'inner_diameter': 1.83e-3, 'outer_diameter': 2.39e-3,
+                    'stiffness': 75e+9, 'torsional_stiffness': 25e+9, 'x_curvature': 10.8, 'y_curvature': 0
+                    }
             }
+            # Unknown tube parameters or where they are from
+            #'tube_parameters': {
+            #    'tube_0':
+            #        {'length': 215e-3, 'length_curved': 14.9e-3, 'inner_diameter': 1.0e-3, 'outer_diameter': 2.4e-3,
+            #         'stiffness': 5e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 15.82, 'y_curvature': 0,
+            #         'k': 16.0},
+
+            #    'tube_1':
+            #        {'length': 120.2e-3, 'length_curved': 21.6e-3, 'inner_diameter': 3.0e-3, 'outer_diameter': 3.8e-3,
+            #         'stiffness': 5e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 11.8, 'y_curvature': 0,
+            #         'k': 9.0},
+
+            #    'tube_2':
+            #        {'length': 48.5e-3, 'length_curved': 8.8e-3, 'inner_diameter': 4.4e-3, 'outer_diameter': 5.4e-3,
+            #         'stiffness': 5.0e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 20.04, 'y_curvature': 0,
+            #         'k': 4.0}
+            #},
         },
         'action_length_limit': 0.001,
         'action_rotation_limit': 5,
