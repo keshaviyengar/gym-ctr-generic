826803bb8dd0e2dcfc256763775cb3e8702b04f5
Keshav Iyengar <keshav.iyengar94@gmail.com>
2021-11-03 10:31:22 +0000

    fixed typo in system 3.

--- ctr_generic_envs/__init__.py
+++ ctr_generic_envs/__init__.py
@@ -57,19 +57,16 @@ register(
             # Unknown tube parameters or where they are from
             'ctr_3': {
                 'tube_0':
-                    {'length': 150e-3, 'length_curved': 100e-3, 'inner_diameter': 1.0e-3, 'outer_diameter': 2.4e-3,
-                     'stiffness': 5e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 15.82, 'y_curvature': 0,
-                     'k': 16.0},
+                    {'length': 150e-3, 'length_curved': 100e-3, 'diameter_inner': 1.0e-3, 'diameter_outer': 2.4e-3,
+                     'stiffness': 5e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 15.82, 'y_curvature': 0},
 
                 'tube_1':
-                    {'length': 100e-3, 'length_curved': 21.6e-3, 'inner_diameter': 3.0e-3, 'outer_diameter': 3.8e-3,
-                     'stiffness': 5e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 11.8, 'y_curvature': 0,
-                     'k': 9.0},
+                    {'length': 100e-3, 'length_curved': 21.6e-3, 'diameter_inner': 3.0e-3, 'diameter_outer': 3.8e-3,
+                     'stiffness': 5e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 11.8, 'y_curvature': 0},
 
                 'tube_2':
-                    {'length': 70e-3, 'length_curved': 8.8e-3, 'inner_diameter': 4.4e-3, 'outer_diameter': 5.4e-3,
-                     'stiffness': 5.0e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 20.04, 'y_curvature': 0,
-                     'k': 4.0}
+                    {'length': 70e-3, 'length_curved': 8.8e-3, 'diameter_inner': 4.4e-3, 'diameter_outer': 5.4e-3,
+                     'stiffness': 5.0e+10, 'torsional_stiffness': 2.3e+10, 'x_curvature': 20.04, 'y_curvature': 0}
             },
         },
         'action_length_limit': 0.001,
